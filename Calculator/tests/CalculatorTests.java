00d3d685e355da3f5da48c0f11fc0424698231db
varadeth <varadetejas@gmail.com>
2021-02-12 13:39:29 +0530

    Refactored code for unit test

--- Calculator/tests/CalculatorTests.java
+++ Calculator/tests/CalculatorTests.java
@@ -6,14 +6,18 @@ public class CalculatorTests {
 
 	@Test
 	public void additionWithEmptyString() {
-		Calculator calculator = new Calculator();
+		Calculator calculator = createCalculator();
 		assertEquals(0, calculator.add(""));
 	}
 	
 	@Test
 	public void additionWithOneNumber() {
-		Calculator calculator = new Calculator();
+		Calculator calculator = createCalculator();
 		assertEquals(1, calculator.add("1"));
+	}
+
+	private Calculator createCalculator() {
+		return new Calculator();
 	}
 
 }
