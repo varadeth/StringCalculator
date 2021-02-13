28e6d1a44665f570331a72de8d5a576d9b739ff4
varadeth <varadetejas@gmail.com>
2021-02-13 12:57:53 +0530

    Added code for adding 2 numbers

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Calculator {
 
 	public int add(String stringOfNumbers) {
 		if(stringOfNumbers.equals(""))
 			return 0;
-		return Integer.parseInt(stringOfNumbers);
+		if(stringOfNumbers.contains(",")) {
+			String[] numbers = stringOfNumbers.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
+		return 1;
 	}
-	
 }
