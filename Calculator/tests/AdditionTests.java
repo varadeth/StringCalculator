78c76b9c184e946b5353599509862314bd1002ce
varadeth <varadetejas@gmail.com>
2021-02-19 18:13:31 +0530

    Renamed CalculatorTests to AdditionTests

--- Calculator/tests/AdditionTests.java
+++ Calculator/tests/AdditionTests.java
@@ -6,7 +6,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class CalculatorTests {
+public class AdditionTests {
 	
 	Calculator calculator;
 	
